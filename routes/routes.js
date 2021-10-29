0e150efa5e681bb54b396018c13c9dd77c1393da
Rina Volovich <rinav@Rinas-MacBook-Air.local>
2021-10-29 19:09:15 +0300

    added get root

--- routes/routes.js
+++ routes/routes.js
@@ -22,18 +22,22 @@ const isAuthenticatedAPI = async (req, res, next) => {
 
 const router = express.Router();
 
-//need post because will receive post request from client
-router.post("/api/register", register);
-router.post("/api/login", login);
-router.post("/api/save", isAuthenticatedAPI, save);
+router.get('/', (req, res, next) => {
+	res.send('API Status: Running');
+});
 
-router.get("/api/checkToken", isAuthenticatedAPI, (req, res) => {
+//need post because will receive post request from client
+router.post("register", register);
+router.post("/login", login);
+router.post("/save", isAuthenticatedAPI, save);
+
+router.get("/checkToken", isAuthenticatedAPI, (req, res) => {
   return res.json(req.body);
 });
 
-router.get("/api/search_history", isAuthenticatedAPI, searchHistory);
+router.get("/search_history", isAuthenticatedAPI, searchHistory);
 
-router.post("/api/logout", (req, res) => {
+router.post("/logout", (req, res) => {
   res.clearCookie("auth");
   return res.json(req.body);
 });
