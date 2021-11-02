1be9f92b5324bcdc997e6ca38d0c1d69a97a2daa
Rina Volovich <rinav@Rinas-MacBook-Air.local>
2021-11-02 21:38:24 +0200

    debug

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
 	res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://red-avert-client.herokuapp.com"
+    "https://red-avert-client.herokuapp.com/"
   );
 	console.log('here')
   console.log(res);
